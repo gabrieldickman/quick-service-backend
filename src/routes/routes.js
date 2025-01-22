41878e87ec8496901ad2a58ef2df90f3f4e4636c
Gabriel Dickman <gabrieldickman33@gmail.com>
2025-01-22 17:09:38 -0400

    feat: add rota para busca de cadastro do cliente

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -3,12 +3,14 @@ const route = express.Router();
 
 const radusuariosController = require("../controllers/ixcControllers/radusuariosController");
 const clientecontratoController = require("../controllers/ixcControllers/clientecontratoController");
+const clienteController = require("../controllers/ixcControllers/clienteController");
 
 const opaController = require("../controllers/opaControllers/opaController");
 const atendimentoController = require("../controllers/opaControllers/atendimentoController");
 
 route.get("/cliente/radius", radusuariosController);
 route.get("/cliente/plano", clientecontratoController);
+route.get("/cliente/cadastro", clienteController);
 route.get("/opa", opaController);
 route.get("/opa/atendimento", atendimentoController);
 
