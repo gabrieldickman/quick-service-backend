42c1a0c8b90a2104bf6af4872a34fec87ec68099
Gabriel Dickman <gabrieldickman33@gmail.com>
2025-02-18 09:12:38 -0400

    clean: removendo comentários des

--- src/controllers/activeDirectory/authController.js
+++ src/controllers/activeDirectory/authController.js
@@ -6,12 +6,10 @@ exports.user_authenticate = (req, res) => {
       
   const { user, password } = req.body;
 
-  // Verifica se o usuário e a senha foram fornecidos
   if (!user || !password) {
     return res.status(400).json({ message: "Usuário e senha são obrigatórios." });
   }
-
-  // Formata o nome de usuário com o domínio (ex: "DOMINIO\\usuario")
+  
   const usernameWithDomain = `${process.env.AD_DOMAIN}\\${user}`;
 
   // Primeira tentativa de autenticação com o servidor AD principal
@@ -21,16 +19,16 @@ exports.user_authenticate = (req, res) => {
       return res.status(500).json({ message: "Erro de conexão com o Active Directory." });
     }
 
-    // Se a autenticação for bem-sucedida, gera o token JWT
     if (auth) {
       return res.status(200).json({
         message: "Autenticação bem-sucedida!",
-        token: jwt.sign({ user }, process.env.JWT_SECRET, { expiresIn: "1h" }), // Gera o token com expiração de 1 hora
+        token: jwt.sign({ user }, process.env.JWT_SECRET, { expiresIn: "1h" }),
       });
     }
 
     // Se a autenticação falhar com o primeiro servidor, tenta o segundo
     console.error("Erro de autenticação no AD (IP 1) - Tentando fallback...");
+
     const adFallback = switchADServer();
 
     adFallback.authenticate(usernameWithDomain, password, (err2, auth2) => {
@@ -39,7 +37,6 @@ exports.user_authenticate = (req, res) => {
         return res.status(500).json({ message: "Erro de conexão com o Active Directory." });
       }
 
-      // Se a autenticação for bem-sucedida com o segundo servidor, gera o token JWT
       if (auth2) {
         return res.status(200).json({
           message: "Autenticação bem-sucedida!",
@@ -47,7 +44,6 @@ exports.user_authenticate = (req, res) => {
         });
       }
 
-      // Se não conseguir autenticar em nenhum servidor
       console.error("Credenciais inválidas após tentativa de fallback.");
       return res.status(401).json({ message: "Credenciais inválidas." });
     });
